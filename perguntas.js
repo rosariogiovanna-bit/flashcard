e8680c3073f00c7f4c8528dd53374bd02ca852eb
rosariogiovanna-bit <rosario.giovanna@escola.pr.gov.br>
2026-07-03 13:52:28 -0300

    qualquer coisa

--- perguntas.js
+++ perguntas.js
@@ -22,3 +22,13 @@ criaCartao(
     'Oi em ingles é HI (RAI)'
 )
 
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
